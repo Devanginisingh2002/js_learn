6929a143113dd8246889cacf34b57a956410f8e7
Devangini Singh <84927553+Devanginisingh2002@users.noreply.github.com>
2024-09-11 12:05:07 +0000

    variables

--- learn_js/test.js
+++ learn_js/test.js
@@ -1,1 +1,24 @@
 console.log("Devangini");
+
+const accountId = 12121
+let accountEmail = "dev@gmail.com"
+var accountPassword = "12345"
+accountCity = "Banglore"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
